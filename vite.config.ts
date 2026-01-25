c79c6850e8d4692ca8ae8dc74f5c5ba90f8950ee
Watcharaphon pamayayang <filmmagic45@gmail.com>
2026-01-25 18:05:05 +0700

    eletron

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
-        port: 3000,
+        port: 5173,
         host: '0.0.0.0',
       },
       plugins: [react()],
@@ -18,6 +18,11 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      base: './', // Important for Electron
+      build: {
+        outDir: 'dist',
+        emptyOutDir: true,
       }
     };
 });
