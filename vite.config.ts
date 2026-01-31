3753ac155a10867b391ccd2c8ebfb84c77d5301c
Watcharaphon pamayayang <filmmagic45@gmail.com>
2026-01-31 22:40:00 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,13 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 5173,
       host: "0.0.0.0",
+      proxy: {
+        '/api': {
+          target: 'http://localhost:7860',
+          changeOrigin: true,
+          secure: false,
+        }
+      }
     },
     plugins: [react(), tailwindcss()],
     define: {
