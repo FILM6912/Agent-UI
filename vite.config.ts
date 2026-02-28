762fa9218524396d0bec4277d2f1a42d3ce0974b
Watcharaphon pamayayang <filmmagic45@gmail.com>
2026-03-01 00:35:05 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -29,22 +29,22 @@ export default defineConfig(({ mode }) => {
             });
           },
         },
-        '/api/v1': {
-          target: 'http://192.168.99.1:7860',
-          changeOrigin: true,
-          secure: false,
-          configure: (proxy, _options) => {
-            proxy.on('error', (err, _req, _res) => {
-              console.log('proxy error', err);
-            });
-            proxy.on('proxyReq', (proxyReq, req, _res) => {
-              console.log('Sending Request to the Target:', req.method, req.url);
-            });
-            proxy.on('proxyRes', (proxyRes, req, _res) => {
-              console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-            });
-          },
-        }
+        // '/api/v1': {
+        //   target: 'http://192.168.99.1:7860',
+        //   changeOrigin: true,
+        //   secure: false,
+        //   configure: (proxy, _options) => {
+        //     proxy.on('error', (err, _req, _res) => {
+        //       console.log('proxy error', err);
+        //     });
+        //     proxy.on('proxyReq', (proxyReq, req, _res) => {
+        //       console.log('Sending Request to the Target:', req.method, req.url);
+        //     });
+        //     proxy.on('proxyRes', (proxyRes, req, _res) => {
+        //       console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+        //     });
+        //   },
+        // }
       },
     },
     plugins: [react(), tailwindcss()],
