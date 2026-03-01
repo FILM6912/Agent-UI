0bb29af7b73d16db58b9af8b2a6e13d64f95700f
Watcharaphon pamayayang <filmmagic45@gmail.com>
2026-03-01 17:33:58 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,22 +16,22 @@ export default defineConfig(({ mode }) => {
         ignored: ["**/server/**"],
       },
       proxy: {
-        // '/api/files': {
-        //   target: 'http://127.0.0.1:8000',
-        //   changeOrigin: true,
-        //   secure: false,
-        //   configure: (proxy, _options) => {
-        //     proxy.on('error', (err, _req, _res) => {
-        //       console.log('proxy error', err);
-        //     });
-        //     proxy.on('proxyReq', (proxyReq, req, _res) => {
-        //       console.log('Sending Request to the Target:', req.method, req.url);
-        //     });
-        //     proxy.on('proxyRes', (proxyRes, req, _res) => {
-        //       console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-        //     });
-        //   },
-        // },
+        '/api/files': {
+          target: 'http://127.0.0.1:8000',
+          changeOrigin: true,
+          secure: false,
+          configure: (proxy, _options) => {
+            proxy.on('error', (err, _req, _res) => {
+              console.log('proxy error', err);
+            });
+            proxy.on('proxyReq', (proxyReq, req, _res) => {
+              console.log('Sending Request to the Target:', req.method, req.url);
+            });
+            proxy.on('proxyRes', (proxyRes, req, _res) => {
+              console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+            });
+          },
+        },
         // // Langflow iframe reverse proxy - serves the main HTML page
         '/__langflow__': {
           target: langflowTarget,
